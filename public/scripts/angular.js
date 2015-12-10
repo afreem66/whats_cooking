79bd01e86159c7e515fc4889bd26fcd9d22a85e5
afreem66 <afreem66@gmail.com>
2015-12-10 16:12:28 -0500

    shopping list populated on click and item removes on click

--- public/scripts/angular.js
+++ public/scripts/angular.js
@@ -3,17 +3,28 @@ var app = angular.module('whatsCookingApp', []);
 app.controller('calendarController', ['$http', function ($http) {
   var controller = this;
 
-  this.searchResults = []
-  this.shoppingList = []
+  this.searchResults = [];
   this.day = null;
+  this.searchTerm = null;
   this.recipe = null;
+  this.calendar = {
+    sunday: "",
+    monday: "",
+    tuesday: "",
+    wednesday: "",
+    thursday: "",
+    friday: "",
+    saturday: ""
+  }
+  this.shoppingList = [];
 
   this.newRecipe = function (id, key) {
     controller.searchResults = []
-    $http.get('https://api.edamam.com/search?q=' + controller.recipe + '&app_id=' + id + '&app_key=' + key).then(
+    $http.get('https://api.edamam.com/search?q=' + controller.searchTerm + '&app_id=' + id + '&app_key=' + key).then(
       function (response) {
         for (var i = 0; i < response.data.hits.length; i++) {
           controller.searchResults.push(response.data.hits[i].recipe)
+          controller.searchTerm = null
         }
       }, function (err) {
         console.log(err);
@@ -22,16 +33,21 @@ app.controller('calendarController', ['$http', function ($http) {
   }
 
   this.chooseRecipe = function (index) {
-    controller.recipe = controller.searchResults[index]
-    console.log(controller.recipe);
+    controller.recipe = controller.searchResults[index].label
+    for (var i = 0; i < controller.searchResults[index].ingredientLines.length; i++) {
+      controller.shoppingList.push(controller.searchResults[index].ingredientLines[i])
+      controller.searchTerm = null
+    }
+
+    if (controller.day == "monday") {
+
+    } else
   }
 
-  this.placeRecipe = function (recipeName) {
-      $(".day").click(
-        this.append("<h3>" + recipeName + "</h3>")
-      )
-      console.log("clicked");
-    }
+  this.removeItem = function (index) {
+    controller.shoppingList.splice(index, 1)
+  }
+
   $http.get('/kevinisdumb').then(function (response) {
     controller.appId = response.data.appId
     controller.appKey = response.data.appKey
--- public/scripts/app.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-  $('.results-container').on("click", function () {
-    console.log(this.children);
-  })
-})
